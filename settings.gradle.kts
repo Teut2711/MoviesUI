fb66cd14ce855b6dbac7e6f052515dd0e3929443
Teut2711 <manglavishesh64@gmail.com>
2024-02-24 00:24:57 +0530

    feat: added github fined grain access token to allow reading artifacts of SDK

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,8 +13,8 @@ dependencyResolutionManagement {
         maven {
             url = uri("https://maven.pkg.github.com/Teut2711/TMBDMovies")
             credentials {
-                username =  System.getenv("USERNAME_GITHUB")
-                password =  System.getenv("TOKEN_GITHUB")
+                username =  "Teut2711" //System.getenv("USERNAME_GITHUB")
+                password =  "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" //System.getenv("TOKEN_GITHUB")
             }
         }
 
